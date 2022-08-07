cbbedb84bb6b268139da9fd2bee14b266ff23c52
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-08-07 18:34:21 +0300

    Initial Commit

---
+++ CustomCirculars/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CustomCirculars
+//
+//  Created by Atil Samancioglu on 7.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCirculars/CustomCircularsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCircularsApp.swift
+//  CustomCirculars
+//
+//  Created by Atil Samancioglu on 7.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomCircularsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
